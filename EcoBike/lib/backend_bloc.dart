866d5c788ca28e5c0192c8e0ade63ceb6b572435
erfanfazal <133206350+NaFreee20@users.noreply.github.com>
2025-02-16 22:08:44 +0000

    implemented the automatic fetch data update

--- EcoBike/lib/backend_bloc.dart
+++ EcoBike/lib/backend_bloc.dart
@@ -1,15 +1,27 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class BackendBloc extends Cubit<String> {
-  BackendBloc() : super('');
+  BackendBloc() : super('') {
+    startFetchingData();
+  }
+
+  void startFetchingData() {
+    Timer.periodic(Duration(milliseconds: 200), (timer) async {
+      await fetchData();
+    });
+  }
 
   Future<void> fetchData() async {
-    final response = await http.get(Uri.parse('http://localhost:5000/api/data'));
+    // final response = await http.get(Uri.parse('http://192.168.192.68:5000/api/data'));
+    final response = await http.get(Uri.parse('http://localhost:5000/api/data')); //Due to change
     if (response.statusCode == 200) {
       emit(response.body);
     } else {
       emit('Failed to fetch data');
     }
+    // print("HEKALS");
+
   }
 }
