e84fd4a29e7eb39f3ab885ed6556ba1678405108
James Rennie <bx23600@bristol.ac.uk>
2025-02-16 15:47:12 +0000

    linked frontend and backend

--- EcoBike/back_end/flaskApp.py
+++ EcoBike/back_end/flaskApp.py
@@ -1,12 +1,21 @@
 from flask import Flask, jsonify
+import threading
+import vidAI
 
 app = Flask(__name__)
+
+def run_pipeline():
+    vidAI.start_pipeline()
+
+thread = threading.Thread(target=run_pipeline, daemon=True)
+thread.start()
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     print("received get")
-    data = {'message': 'Hello from Python backend!'}
-    return jsonify(data)
+    # data = vidAI.prediction
+    data = {'message' : 'hello'}
+    return jsonify({'predictions': vidAI.prediction })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
